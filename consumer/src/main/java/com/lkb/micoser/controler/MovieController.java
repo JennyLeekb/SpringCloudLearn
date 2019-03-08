bcbd7056eed09cf01e9c4946ca7e011f9a22b8c2
lkb <cuglkb@163.com>
2019-03-08 16:30:19 +0800

    2019-03-08 ribbon负载均衡

--- consumer/src/main/java/com/lkb/micoser/controler/MovieController.java
+++ consumer/src/main/java/com/lkb/micoser/controler/MovieController.java
@@ -2,12 +2,15 @@ package com.lkb.micoser.controler;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.client.RestTemplate;
 
 import com.lkb.micoser.POJO.User;
@@ -20,10 +23,15 @@ public class MovieController {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 
 	@RequestMapping("consumer/getById")
 	public User getById(int id) {
-		return this.restTemplate.getForObject("http://localhost:8000/user/getById?id=" + id, User.class);
+		return this.restTemplate.getForObject("http://user/user/getById?id=" + id, User.class);
 	}
 
 	/**
@@ -33,6 +41,12 @@ public class MovieController {
 	  @GetMapping("consumer/user")
 	  public List<ServiceInstance> showInfo() {
 	    return this.discoveryClient.getInstances("user");
+	  }
+	  
+	  @GetMapping("/logInstance")
+	  public void logUserInstance() {
+		  ServiceInstance serviceInstance = this.loadBalancerClient.choose("user");
+		  MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
 	  }
 	
 }
